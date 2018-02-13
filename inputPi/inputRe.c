de66a474bb88abcf7570894a04b9157111b8997c
Noah Evans <clonex10100@gmail.com>
2018-02-13 16:45:58 -0600

    made more straightforward printing

--- inputPi/inputRe.c
+++ inputPi/inputRe.c
@@ -47,18 +47,19 @@ int main(void) {
 				}
 			}		
 			if(mods == 0){
-				printf("\\0");
+				//printf("\\0");
+				printf("\\0\\0\\%s\\%s\\%s\\%s\\%s\\%s\n",keys[0],keys[1],keys[2],keys[3],keys[4],keys[5]);
 			}
 			else{
-				printf("\\x%02x",mods);
+				printf("\\0\\x%02x\\%s\\%s\\%s\\%s\\%s\\%s\n",mods,keys[0],keys[1],keys[2],keys[3],keys[4],keys[5]);
 			}
-			printf("\\0");
-			for(int i = 0; i < 6; i++){
-				printf("\\%s",keys[i]);
-			}
-			printf("\n");
-			//print(keys);
+			//printf("\\0");
+			//for(int i = 0; i < 6; i++){
+				//printf("\\%s",keys[i]);
+			//}
+			//printf("\n");
 			//output
+			
 		}
 	}
 }
