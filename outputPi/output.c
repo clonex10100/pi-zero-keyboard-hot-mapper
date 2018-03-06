c2baf51718b59935b1be33771ac22b86debaed41
Noah Evans <clonex10100@gmail.com>
2018-03-06 08:51:07 -0600

    Cleaned up and added binary output

--- outputPi/output.c
+++ outputPi/output.c
@@ -7,22 +7,29 @@
 #include <string.h>
 #include <stdio.h>
 
-char* cToSend(int c);
+int cToSend(int c);
 
 int main(void){
 	char input[26];
-	char out[100];
-
+	//char out[100];
+	
+	//Stores currently pressed keys
 	int keys[6];
 	int mods;
 	
+	//Buffer for parsing serial input
 	char buffer[4];
 	buffer[3] = '\0'; 
 	int bIndex;
-
+	
+	//open keyboard port
+	FILE *out = fopen("/dev/hidg0", "wb");
+	//Open serial port
 	int in = open("/dev/ttyAMA0", O_RDWR | O_NOCTTY);
+	
+	//Setup serial
 	struct termios options;	
-	printf("%i\n",tcgetattr(in, &options));
+	tcgetattr(in, &options);
 	cfmakeraw(&options);
 	cfsetspeed(&options, B9600);
 	options.c_cflag &= ~CSTOPB;
@@ -30,15 +37,17 @@ int main(void){
 	options.c_cflag |= CREAD;
 	options.c_cc[VMIN] = 26;
 	options.c_cc[VTIME] = 0;
-	printf("%i\n",tcsetattr(in,TCSANOW, &options));
+	tcsetattr(in,TCSANOW, &options);
 	tcflush(in, TCIFLUSH);
+	
 	while(1){
 		printf("read\n");
 		printf("%i\n",read(in,input,26));
 		printf("%s\n",input);
+		
+		//Parses input into keys and mods
 		bIndex = 0;
 		for(int i = 0; i < 25; i++){
-			//printf("%c\n",input[i]);
 			if(input[i] == ':'){
 				bIndex = 0;
 				if(i == 3){
@@ -55,125 +64,133 @@ int main(void){
 				bIndex++;
 			}		
 		}
+		
+		//Just for printing. Delete for speed
 		if(mods == 0){
-			sprintf(out,"exec echo -ne \\\\0\\\\0\\\\%s\\\\%s\\\\%s\\\\%s\\\\%s\\\\%s > /dev/hidg0",cToSend(keys[0]),cToSend(keys[1]),cToSend(keys[2]),cToSend(keys[3]),cToSend(keys[4]),cToSend(keys[5]));
+			printf("\\\\0\\\\0\\\\%s\\\\%s\\\\%s\\\\%s\\\\%s\\\\%s",cToSend(keys[0]),cToSend(keys[1]),cToSend(keys[2]),cToSend(keys[3]),cToSend(keys[4]),cToSend(keys[5]));
 							                              
 		}
 		else{
-			sprintf(out,"exec echo -ne \\\\x%02x\\\\0\\\\%s\\\\%s\\\\%s\\\\%s\\\\%s\\\\%s > /dev/hidg0",mods,cToSend(keys[0]),cToSend(keys[1]),cToSend(keys[2]),cToSend(keys[3]),cToSend(keys[4]),cToSend(keys[5]));
+			printf("\\\\x%02x\\\\0\\\\%s\\\\%s\\\\%s\\\\%s\\\\%s\\\\%s",mods,cToSend(keys[0]),cToSend(keys[1]),cToSend(keys[2]),cToSend(keys[3]),cToSend(keys[4]),cToSend(keys[5]));
 		}
-		printf("%s\n",out);
-		system(out);
+		output(out, mods, keys[]);
 	}
 	
 }
-
-char* cToSend(int c){
+void output(FILE out, int mods, int keys[6]){
+	int zero = 0;
+	fwrite(&mods, 1, 1, out);
+	fwrite(&zero, 1, 1, out);
+	for(int i = 0; i < 6; i++){
+		fwrite(&cToSend(keys[i]), 1, 1, out);
+	}
+}
+int cToSend(int c){
         switch(c){
 		case KEY_1:
-			return "x1e";
+			return 0x1e;
 		case KEY_2:
-			return "x1f";
+			return 0x1f;
 		case KEY_3:
-			return "x20";
+			return 0x20;
 		case KEY_4:
-			return "x21";
+			return 0x21;
 		case KEY_5:
-			return "x22";
+			return 0x22;
 		case KEY_6:
-			return "x23";
+			return 0x23;
 		case KEY_7:
-			return "x24";
+			return 0x24;
 		case KEY_8:
-			return "x25";
+			return 0x25;
 		case KEY_9:
-			return "x26";
+			return 0x26;
 		case KEY_0:
-			return "x27";
+			return 0x27;
                 case KEY_A:
-                        return "x04";
+                        return 0x04;
                 case KEY_B:
-                        return "x05";
+                        return 0x05;
                 case KEY_C:
-                        return "x06";
+                        return 0x06;
                 case KEY_D:
-                        return "x07";
+                        return 0x07;
                 case KEY_E:
-                        return "x08";
+                        return 0x08;
                 case KEY_F:
-                        return "x09";
+                        return 0x09;
                 case KEY_G:
-                        return "x0a";
+                        return 0x0a;
                 case KEY_H:
-                        return "x0b";
+                        return 0x0b;
                 case KEY_I:
-                        return "x0c";
+                        return 0x0c;
                 case KEY_J:
-                        return "x0d";
+                        return 0x0d;
                 case KEY_K:
-                        return "x0e";
+                        return 0x0e;
                 case KEY_L:
-                        return "x0f";
+                        return 0x0f;
                 case KEY_M:
-                        return "x10";
+                        return 0x10;
                 case KEY_N:
-                        return "x11";
+                        return 0x11;
                 case KEY_O:
-                        return "x12";
+                        return 0x12;
                 case KEY_P:
-                        return "x13";
+                        return 0x13;
                 case KEY_Q:
-                        return "x14";
+                        return 0x14;
                 case KEY_R:
-                        return "x15";
+                        return 0x15;
                 case KEY_S:
-                        return "x16";
+                        return 0x16;
                 case KEY_T:
-                        return "x17";
+                        return 0x17;
                 case KEY_U:
-                        return "x18";
+                        return 0x18;
                 case KEY_V:
-                        return "x19";
+                        return 0x19;
                 case KEY_W:
-                        return "x1a";
+                        return 0x1a;
                 case KEY_X:
-                        return "x1b";
+                        return 0x1b;
                 case KEY_Y:
-                        return "x1c";
+                        return 0x1c;
                 case KEY_Z:
-                        return "x1d";
+                        return 0x1d;
 		case KEY_ENTER:
-			return "x28";
+			return 0x28;
 		case KEY_ESC:
-			return "x29";
+			return 0x29;
 		case KEY_BACKSPACE:
-			return "x2a";
+			return 0x2a;
 		case KEY_TAB:
-			return "x2b";
+			return 0x2b;
 		case KEY_SPACE:
-			return "x2c";
+			return 0x2c;
 		case KEY_MINUS:
-			return "x2d";
+			return 0x2d;
 		case KEY_EQUAL:
-			return "x2e";
+			return 0x2e;
 		case KEY_LEFTBRACE:
-			return "x2f";
+			return 0x2f;
 		case KEY_RIGHTBRACE:
-			return "x30";
+			return 0x30;
 		case KEY_BACKSLASH:
-			return "x31";
+			return 0x31;
 		case KEY_SEMICOLON:
-			return "x33";
+			return 0x33;
 		case KEY_APOSTROPHE:
-			return "x34";
+			return 0x34;
 		case KEY_GRAVE:
-			return "x35";
+			return 0x35;
 		case KEY_COMMA:
-			return "x36";
+			return 0x36;
 		case KEY_DOT:
-			return "x37";
+			return 0x37;
 		case KEY_SLASH:
-			return "x38";
+			return 0x38;
                 default:
                         return "0";
     }
